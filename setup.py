db1326f1346c8f503b8b4f21f12526ed8fd48d0c
rgerkin <rgerkin@asu.edu>
2020-05-22 21:54:52 +0000

    Converted to an installable package

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""Setup file for PyRCCA"""
+
+from setuptools import setup, find_packages
+from pathlib import Path
+
+readme_path = Path(__file__).parent / 'README.md'
+with open(readme_path, encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyrcca',
+    version=0.1,
+    author='Bilenko et al',
+    packages=find_packages(),
+    url = 'https://github.com/gallantlab/pyrcca',
+    license='MIT',
+    description='Regularized kernel canonical correlation analysis in Python.',
+    long_description=long_description,
+    install_requires=[
+	'h5py',
+	'numpy',
+	'scipy',
+    	]
+    )
