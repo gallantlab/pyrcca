bd0c2e96b60ace56057fc0cb568b123101ded5fb
Robin Thibaut <robin.thibaut@ugent.be>
2021-06-15 15:07:29 +0200

    Reformatting

--- rcca/__init__.py
+++ rcca/__init__.py
@@ -5,14 +5,25 @@ import joblib
 import numpy as np
 from scipy.linalg import eigh
 
-
-__copyright__ = 'Copyright 2016, UC Berkeley, Gallant lab.'
+__copyright__ = "Copyright 2016, UC Berkeley, Gallant lab."
 
 
 class _CCABase(object):
-    def __init__(self, numCV=None, reg=None, regs=None, numCC=None,
-                 numCCs=None, kernelcca=True, ktype=None, verbose=False,
-                 select=0.2, cutoff=1e-15, gausigma=1.0, degree=2):
+    def __init__(
+            self,
+            numCV=None,
+            reg=None,
+            regs=None,
+            numCC=None,
+            numCCs=None,
+            kernelcca=True,
+            ktype=None,
+            verbose=False,
+            select=0.2,
+            cutoff=1e-15,
+            gausigma=1.0,
+            degree=2,
+    ):
         self.numCV = numCV
         self.reg = reg
         self.regs = regs
@@ -24,29 +35,38 @@ class _CCABase(object):
         self.select = select
         self.gausigma = gausigma
         self.degree = degree
-        if self.kernelcca and self.ktype == None:
-            self.ktype = 'linear'
+        if self.kernelcca and self.ktype is None:
+            self.ktype = "linear"
         self.verbose = verbose
 
     def train(self, data):
         nT = data[0].shape[0]
         if self.verbose:
-            print('Training CCA, kernel = %s, regularization = %0.4f, '
-                  '%d components' % (self.ktype, self.reg, self.numCC))
+            print(
+                "Training CCA, kernel = %s, regularization = %0.4f, "
+                "%d components" % (self.ktype, self.reg, self.numCC)
+            )
 
-        comps = kcca(data, self.reg, self.numCC, kernelcca=self.kernelcca,
-                     ktype=self.ktype, gausigma=self.gausigma,
-                     degree=self.degree)
-        self.cancorrs, self.ws, self.comps = recon(data, comps,
-                                                   kernelcca=self.kernelcca)
+        comps = kcca(
+            data,
+            self.reg,
+            self.numCC,
+            kernelcca=self.kernelcca,
+            ktype=self.ktype,
+            gausigma=self.gausigma,
+            degree=self.degree,
+        )
+        self.cancorrs, self.ws, self.comps = recon(
+            data, comps, kernelcca=self.kernelcca
+        )
         if len(data) == 2:
             self.cancorrs = self.cancorrs[np.nonzero(self.cancorrs)]
         return self
 
     def validate(self, vdata):
         vdata = [np.nan_to_num(_zscore(d)) for d in vdata]
-        if not hasattr(self, 'ws'):
-            raise NameError('Algorithm has not been trained.')
+        if not hasattr(self, "ws"):
+            raise NameError("Algorithm has not been trained.")
         self.preds, self.corrs = predict(vdata, self.ws, self.cutoff)
         return self.corrs
 
@@ -57,25 +77,26 @@ class _CCABase(object):
         nF = [d.shape[1] for d in vdata]
         self.ev = [np.zeros((nC, f)) for f in nF]
         for cc in range(nC):
-            ccs = cc+1
+            ccs = cc + 1
             if self.verbose:
-                print('Computing explained variance for component #%d' % ccs)
-            preds, corrs = predict(vdata, [w[:, ccs-1:ccs] for w in self.ws],
-                                   self.cutoff)
-            resids = [abs(d[0]-d[1]) for d in zip(vdata, preds)]
+                print("Computing explained variance for component #%d" % ccs)
+            preds, corrs = predict(
+                vdata, [w[:, ccs - 1: ccs] for w in self.ws], self.cutoff
+            )
+            resids = [abs(d[0] - d[1]) for d in zip(vdata, preds)]
             for s in range(nD):
-                ev = abs(vdata[s].var(0) - resids[s].var(0))/vdata[s].var(0)
-                ev[np.isnan(ev)] = 0.
+                ev = abs(vdata[s].var(0) - resids[s].var(0)) / vdata[s].var(0)
+                ev[np.isnan(ev)] = 0.0
                 self.ev[s][cc] = ev
         return self.ev
 
     def save(self, filename):
-        h5 = h5py.File(filename, 'a')
+        h5 = h5py.File(filename, "a")
         for key, value in self.__dict__.items():
             if value is not None:
                 if isinstance(value, list):
                     for di in range(len(value)):
-                        grpname = 'dataset%d' % di
+                        grpname = "dataset%d" % di
                         dgrp = h5.require_group(grpname)
                         try:
                             dgrp.create_dataset(key, data=value[di])
@@ -87,11 +108,11 @@ class _CCABase(object):
         h5.close()
 
     def load(self, filename):
-        h5 = h5py.File(filename, 'a')
+        h5 = h5py.File(filename, "a")
         for key, value in h5.attrs.items():
             setattr(self, key, value)
         for di in range(len(h5.keys())):
-            ds = 'dataset%d' % di
+            ds = "dataset%d" % di
             for key, value in h5[ds].items():
                 if di == 0:
                     setattr(self, key, [])
@@ -121,19 +142,34 @@ class CCACrossValidate(_CCABase):
         ev (list): explained variance for each canonical dimension
     """
 
-    def __init__(self, numCV=None, regs=None, numCCs=None, kernelcca=True,
-                 ktype=None, verbose=True, select=0.2, cutoff=1e-15,
-                 gausigma=1.0, degree=2):
+    def __init__(
+            self,
+            numCV=None,
+            regs=None,
+            numCCs=None,
+            kernelcca=True,
+            ktype=None,
+            verbose=True,
+            select=0.2,
+            cutoff=1e-15,
+            gausigma=1.0,
+            degree=2,
+    ):
         numCV = 10 if numCV is None else numCV
         regs = np.array(np.logspace(-3, 1, 10)) if regs is None else regs
         numCCs = np.arange(5, 10) if numCCs is None else numCCs
-        super(CCACrossValidate, self).__init__(numCV=numCV, regs=regs,
-                                               numCCs=numCCs,
-                                               kernelcca=kernelcca,
-                                               ktype=ktype, verbose=verbose,
-                                               select=select, cutoff=cutoff,
-                                               gausigma=gausigma,
-                                               degree=degree)
+        super(CCACrossValidate, self).__init__(
+            numCV=numCV,
+            regs=regs,
+            numCCs=numCCs,
+            kernelcca=kernelcca,
+            ktype=ktype,
+            verbose=verbose,
+            select=select,
+            cutoff=cutoff,
+            gausigma=gausigma,
+            degree=degree,
+        )
 
     def train(self, data, parallel=True):
         """
@@ -154,23 +190,36 @@ class CCACrossValidate(_CCABase):
         selection = max(int(self.select * min([d.shape[1] for d in data])), 1)
         for ri, reg in enumerate(self.regs):
             for ci, numCC in enumerate(self.numCCs):
-                running_corr_mean_sum = 0.
+                running_corr_mean_sum = 0.0
                 if parallel:
                     fold_corr_means = joblib.Parallel(n_jobs=self.numCV)(
                         joblib.delayed(train_cvfold)(
-                            data=data, reg=reg, numCC=numCC,
-                            kernelcca=self.kernelcca, ktype=self.ktype,
-                            gausigma=self.gausigma, degree=self.degree,
-                            cutoff=self.cutoff, selection=selection)
-                        for fold in range(self.numCV))
+                            data=data,
+                            reg=reg,
+                            numCC=numCC,
+                            kernelcca=self.kernelcca,
+                            ktype=self.ktype,
+                            gausigma=self.gausigma,
+                            degree=self.degree,
+                            cutoff=self.cutoff,
+                            selection=selection,
+                        )
+                        for fold in range(self.numCV)
+                    )
                     running_corr_mean_sum += sum(fold_corr_means)
                 else:
                     for cvfold in range(self.numCV):
                         fold_corr_mean = train_cvfold(
-                            data=data, reg=reg, numCC=numCC,
-                            kernelcca=self.kernelcca, ktype=self.ktype,
-                            gausigma=self.gausigma, degree=self.degree,
-                            cutoff=self.cutoff, selection=selection)
+                            data=data,
+                            reg=reg,
+                            numCC=numCC,
+                            kernelcca=self.kernelcca,
+                            ktype=self.ktype,
+                            gausigma=self.gausigma,
+                            degree=self.degree,
+                            cutoff=self.cutoff,
+                            selection=selection,
+                        )
                         running_corr_mean_sum += fold_corr_mean
 
                 corr_mat[ri, ci] = running_corr_mean_sum / self.numCV
@@ -178,17 +227,26 @@ class CCACrossValidate(_CCABase):
         self.best_reg = self.regs[best_ri[0]]
         self.best_numCC = self.numCCs[best_ci[0]]
 
-        comps = kcca(data, self.best_reg, self.best_numCC,
-                     kernelcca=self.kernelcca, ktype=self.ktype,
-                     gausigma=self.gausigma, degree=self.degree)
-        self.cancorrs, self.ws, self.comps = recon(data, comps,
-                                                   kernelcca=self.kernelcca)
+        comps = kcca(
+            data,
+            self.best_reg,
+            self.best_numCC,
+            kernelcca=self.kernelcca,
+            ktype=self.ktype,
+            gausigma=self.gausigma,
+            degree=self.degree,
+        )
+        self.cancorrs, self.ws, self.comps = recon(
+            data, comps, kernelcca=self.kernelcca
+        )
         if len(data) == 2:
             self.cancorrs = self.cancorrs[np.nonzero(self.cancorrs)]
         return self
 
-def train_cvfold(data, reg, numCC, kernelcca, ktype, gausigma, degree,
-                 cutoff, selection):
+
+def train_cvfold(
+        data, reg, numCC, kernelcca, ktype, gausigma, degree, cutoff, selection
+):
     """
     Train a cross-validation fold of CCA
     """
@@ -197,14 +255,20 @@ def train_cvfold(data, reg, numCC, kernelcca, ktype, gausigma, degree,
     nchunks = int(0.2 * nT / chunklen)
     indchunks = list(zip(*[iter(range(nT))] * chunklen))
     np.random.shuffle(indchunks)
-    heldinds = [ind for chunk in indchunks[:nchunks]
-                for ind in chunk]
+    heldinds = [ind for chunk in indchunks[:nchunks] for ind in chunk]
     notheldinds = list(set(range(nT)) - set(heldinds))
-    comps = kcca([d[notheldinds] for d in data], reg, numCC,
-                 kernelcca=kernelcca, ktype=ktype,
-                 gausigma=gausigma, degree=degree)
-    cancorrs, ws, ccomps = recon([d[notheldinds] for d in data], comps,
-                                 kernelcca=kernelcca)
+    comps = kcca(
+        [d[notheldinds] for d in data],
+        reg,
+        numCC,
+        kernelcca=kernelcca,
+        ktype=ktype,
+        gausigma=gausigma,
+        degree=degree,
+    )
+    cancorrs, ws, ccomps = recon(
+        [d[notheldinds] for d in data], comps, kernelcca=kernelcca
+    )
     preds, corrs = predict([d[heldinds] for d in data], ws, cutoff=cutoff)
     fold_corr_mean = []
     for corr in corrs:
@@ -232,10 +296,18 @@ class CCA(_CCABase):
         preds (list): predictions on the validation dataset
         ev (list): explained variance for each canonical dimension
     """
-    def __init__(self, reg=0., numCC=10, kernelcca=True, ktype=None,
-                 verbose=True, cutoff=1e-15):
-        super(CCA, self).__init__(reg=reg, numCC=numCC, kernelcca=kernelcca,
-                                  ktype=ktype, verbose=verbose, cutoff=cutoff)
+
+    def __init__(
+            self, reg=0.0, numCC=10, kernelcca=True, ktype=None, verbose=True, cutoff=1e-15
+    ):
+        super(CCA, self).__init__(
+            reg=reg,
+            numCC=numCC,
+            kernelcca=kernelcca,
+            ktype=ktype,
+            verbose=verbose,
+            cutoff=cutoff,
+        )
 
     def train(self, data):
         return super(CCA, self).train(data)
@@ -262,13 +334,14 @@ def predict(vdata, ws, cutoff=1e-15):
     return preds, corrs
 
 
-def kcca(data, reg=0., numCC=None, kernelcca=True, ktype='linear',
-         gausigma=1.0, degree=2):
-    """Set up and solve the kernel CCA eigenproblem
-    """
+def kcca(
+        data, reg=0.0, numCC=None, kernelcca=True, ktype="linear", gausigma=1.0, degree=2
+):
+    """Set up and solve the kernel CCA eigenproblem"""
     if kernelcca:
-        kernel = [_make_kernel(d, ktype=ktype, gausigma=gausigma,
-                               degree=degree) for d in data]
+        kernel = [
+            _make_kernel(d, ktype=ktype, gausigma=gausigma, degree=degree) for d in data
+        ]
     else:
         kernel = [d.T for d in data]
 
@@ -285,17 +358,18 @@ def kcca(data, reg=0., numCC=None, kernelcca=True, ktype='linear',
 
     # Fill the left and right sides of the eigenvalue problem
     for i in range(nDs):
-        RH[sum(nFs[:i]) : sum(nFs[:i+1]),
-           sum(nFs[:i]) : sum(nFs[:i+1])] = (crosscovs[i * (nDs + 1)]
-                                             + reg * np.eye(nFs[i]))
+        RH[
+        sum(nFs[:i]): sum(nFs[: i + 1]), sum(nFs[:i]): sum(nFs[: i + 1])
+        ] = crosscovs[i * (nDs + 1)] + reg * np.eye(nFs[i])
 
         for j in range(nDs):
             if i != j:
-                LH[sum(nFs[:j]) : sum(nFs[:j+1]),
-                   sum(nFs[:i]) : sum(nFs[:i+1])] = crosscovs[nDs * j + i]
+                LH[
+                sum(nFs[:j]): sum(nFs[: j + 1]), sum(nFs[:i]): sum(nFs[: i + 1])
+                ] = crosscovs[nDs * j + i]
 
-    LH = (LH + LH.T) / 2.
-    RH = (RH + RH.T) / 2.
+    LH = (LH + LH.T) / 2.0
+    RH = (RH + RH.T) / 2.0
 
     maxCC = LH.shape[0]
     r, Vs = eigh(LH, RH, eigvals=(maxCC - numCC, maxCC - 1))
@@ -304,7 +378,7 @@ def kcca(data, reg=0., numCC=None, kernelcca=True, ktype='linear',
     comp = []
     Vs = Vs[:, rindex]
     for i in range(nDs):
-        comp.append(Vs[sum(nFs[:i]):sum(nFs[:i + 1]), :numCC])
+        comp.append(Vs[sum(nFs[:i]): sum(nFs[: i + 1]), :numCC])
     return comp
 
 
@@ -322,24 +396,29 @@ def recon(data, comp, corronly=False, kernelcca=True):
     else:
         return corrs, ws, ccomp
 
-def _zscore(d): return (d - d.mean(0)) / d.std(0)
+
+def _zscore(d):
+    return (d - d.mean(0)) / d.std(0)
 
 
-def _demean(d): return d - d.mean(0)
+def _demean(d):
+    return d - d.mean(0)
 
 
-def _listdot(d1, d2): return [np.dot(x[0].T, x[1]) for x in zip(d1, d2)]
+def _listdot(d1, d2):
+    return [np.dot(x[0].T, x[1]) for x in zip(d1, d2)]
 
 
 def _listcorr(a):
-    """Returns pairwise row correlations for all items in array as a list of matrices
-    """
+    """Returns pairwise row correlations for all items in array as a list of matrices"""
     corrs = np.zeros((a[0].shape[1], len(a), len(a)))
     for i in range(len(a)):
         for j in range(len(a)):
             if j > i:
-                corrs[:, i, j] = [np.nan_to_num(np.corrcoef(ai, aj)[0, 1])
-                                  for (ai, aj) in zip(a[i].T, a[j].T)]
+                corrs[:, i, j] = [
+                    np.nan_to_num(np.corrcoef(ai, aj)[0, 1])
+                    for (ai, aj) in zip(a[i].T, a[j].T)
+                ]
     return corrs
 
 
@@ -351,23 +430,24 @@ def _rowcorr(a, b):
     return cs
 
 
-def _make_kernel(d, normalize=True, ktype='linear', gausigma=1.0, degree=2):
+def _make_kernel(d, normalize=True, ktype="linear", gausigma=1.0, degree=2):
     """Makes a kernel for data d
-      If ktype is 'linear', the kernel is a linear inner product
-      If ktype is 'gaussian', the kernel is a Gaussian kernel, sigma = gausigma
-      If ktype is 'poly', the kernel is a polynomial kernel with degree=degree
+    If ktype is 'linear', the kernel is a linear inner product
+    If ktype is 'gaussian', the kernel is a Gaussian kernel, sigma = gausigma
+    If ktype is 'poly', the kernel is a polynomial kernel with degree=degree
     """
     d = np.nan_to_num(d)
     cd = _demean(d)
-    if ktype == 'linear':
+    if ktype == "linear":
         kernel = np.dot(cd, cd.T)
-    elif ktype == 'gaussian':
+    elif ktype == "gaussian":
         from scipy.spatial.distance import pdist, squareform
-        pairwise_dists = squareform(pdist(d, 'euclidean'))
-        kernel = np.exp(-pairwise_dists ** 2 / 2 * gausigma ** 2)
-    elif ktype == 'poly':
+
+        pairwise_dists = squareform(pdist(d, "euclidean"))
+        kernel = np.exp(-(pairwise_dists ** 2) / 2 * gausigma ** 2)
+    elif ktype == "poly":
         kernel = np.dot(cd, cd.T) ** degree
-    kernel = (kernel + kernel.T) / 2.
+    kernel = (kernel + kernel.T) / 2.0
     if normalize:
         kernel = kernel / np.linalg.eigvalsh(kernel).max()
     return kernel
