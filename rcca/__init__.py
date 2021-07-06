bc60f25a8e395baf495dd0f7234a68490ed16a22
Tom Dupré la Tour <tom.dupre-la-tour@m4x.org>
2021-07-06 11:37:24 -0700

    MNT PyPI version 0.2

--- rcca/__init__.py
+++ rcca/__init__.py
@@ -1,1 +1,3 @@
 from .rcca import *
+
+__version__ = '0.2'
--- setup.py
+++ setup.py
@@ -2,14 +2,24 @@
 
 from setuptools import setup, find_packages
 from pathlib import Path
+import re
 
 readme_path = Path(__file__).parent / 'README.md'
 with open(readme_path, encoding='utf-8') as f:
     long_description = f.read()
 
+# get version from rcca/__init__.py
+__version__ = 0.0
+with open('rcca/__init__.py') as f:
+    infos = f.readlines()
+for line in infos:
+    if "__version__" in line:
+        match = re.search(r"__version__ = '([^']*)'", line)
+        __version__ = match.groups()[0]
+
 setup(
     name='pyrcca',
-    version='0.1',
+    version=__version__,
     author='Bilenko et al',
     packages=find_packages(),
     url='https://github.com/gallantlab/pyrcca',
@@ -22,5 +32,5 @@ setup(
         'numpy',
         'scipy',
         'joblib',
-    ]
+    ],
 )
